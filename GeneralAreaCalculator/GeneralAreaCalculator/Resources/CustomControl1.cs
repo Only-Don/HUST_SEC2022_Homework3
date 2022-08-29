2e74e011925ca8b6ae3ef6fc5bd99189dfb1874a
Only-Don <Only_Don@163.com>
2022-08-29 23:48:41 +0800

    测试文件是否良好，并完善回滚内容，删除多余文件

---
+++ GeneralAreaCalculator/GeneralAreaCalculator/Resources/CustomControl1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeneralAreaCalculator
+{
+    public partial class ShapeButton : Button
+    {
+        public ShapeButton()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+
+        bool flag;
+        [Description(" 获取或设置按钮椭圆效果。"), DefaultValue(false)]
+        public bool Circle
+        {
+            set
+            {
+                flag = value;
+                GraphicsPath gp = new GraphicsPath();
+                gp.AddEllipse(this.ClientRectangle);//圆形
+                this.Region = new Region(gp);
+                FlatAppearance.BorderSize = 0;//去掉边框
+                FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                this.Invalidate();
+            }
+            get { return flag; }
+        }
+    }
+}
